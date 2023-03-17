4cec402134ecbb4c70b8536921a5245a619bf01b
Luis Carlos Moriel Enriquez <carlosmorielenriquez@gmail.com>
2023-03-17 17:24:40 -0600

    Graphics changes

--- src/components/Graphics.js
+++ src/components/Graphics.js
@@ -1,9 +1,21 @@
 import { useEffect, useState } from "react";
 import { getHistoricalKandles } from "../API/GetHistoricalKandles";
 import HistoricalChart from "./Graphics/HistoricalChart";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
 function Graphics() {
-	return <HistoricalChart />;
+	return (
+		<Box sx={{ flexGrow: 1 }} className="graphics">
+			<HistoricalChart />
+			<Grid container spacing={4}>
+				<Grid item xs={4} className="item-statistic">
+					<Item>B1</Item>
+				</Grid>
+			</Grid>
+		</Box>
+	);
 }
 
 export default Graphics;
